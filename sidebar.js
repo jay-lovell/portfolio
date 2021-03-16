7b3a36d5fc714f57cc763e4c073dd1608876e39b
LOVELL <jay.lovell2@admiralgroup.co.uk>
2021-03-16 21:15:56 +0000

    Massive rewrite to improve hierarchy. All good

--- sidebar.js
+++ sidebar.js
@@ -1,7 +1,16 @@
+window.addEventListener("click", function(e){
+  if (!document.getElementById('sidenav').contains(e.target) && (!document.getElementById('nav-icon').contains(e.target))){
+  closeNav();
+   ; //the same code you've used to hide the menu
+} 
+})
+
 function openNav() {
     document.getElementById("sidenav").style.width = "200px";
+    setTimeout(closeNav, 4000);
   }
   
 function closeNav() {
     document.getElementById("sidenav").style.width = "0px";
   }
+
